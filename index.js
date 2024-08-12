50d04e452470eacacc8ceb543af924ce45e7b6f9
[BladrenS] <denis_ka_98@mail.ru>
2024-08-12 21:02:34 +0600

    feat: add categories

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let word = 'random';
 const input = document.querySelector('.input');
 const cross = document.querySelector('.cross');
 const mag = document.querySelector('.mag');
+const categories = document.querySelectorAll('.cat');
+
 // Получаем изображения
 async function getInfo() {
    const information = await fetch(`https://api.unsplash.com/search/photos?query=${word}&per_page=30&extras=url_m&orientation=landscape&client_id=ztaXHAv9liSB6pPIfSpUEXe42MptozHs7v2kNqqX4hM`)
@@ -22,6 +24,7 @@ function showInfo(info) {
    })
 };
 
+// Отправка на enter
 document.onkeydown = (e) => {
    if (e.code === 'Enter') {
       word = input.value;
@@ -29,8 +32,17 @@ document.onkeydown = (e) => {
    };
 }
 
+// Отправка на лупу
 cross.addEventListener('click', () => input.value = '');
 mag.addEventListener('click', () => {
    word = input.value;
    getInfo();
 });
+
+// Кнопочки-категории
+document.addEventListener('click', (e) => {
+   if (e.target.classList[0] === 'cat') {
+      word = e.target.innerHTML;
+      getInfo();
+   }  
+})
